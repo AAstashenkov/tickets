975a93ef3d55f371b8531e747d04b3f3410157b8
AAstashenkov <arsenkaman@gmail.com>
2024-11-18 23:12:27 +0300

    Feat: Custom collections

--- src/main/java/org/asta/Application.java
+++ src/main/java/org/asta/Application.java
@@ -1,31 +1,45 @@
 package org.asta;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.asta.model.BusTicket;
-import org.asta.validator.Validator;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import org.asta.arrayList.ArrayList;
+import org.asta.hashSet.HashSet;
 
 /**
  * @author Asta
  */
+
 public class Application {
-    public static void main(String[] args) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new FileReader("src/main/java/tickets.txt"));
-        BusTicket ticket;
-        Validator validator = new Validator();
-        ObjectMapper mapper = new ObjectMapper();
-        String input;
-        while ((input = reader.readLine()) != null) {
-            ticket = mapper.readValue(input, BusTicket.class);
-            System.out.println(ticket.toString());
-            System.out.println(validator.validateBusTicket(ticket));
-        }
-        validator.getPopularViolation();
+    private static ArrayList arrayList = new ArrayList();
 
+    public static void main(String[] args) {
+
+        System.out.println("Array list:");
+        arrayList.put("One");
+        arrayList.put("Two");
+        arrayList.put("Three");
+
+        showArrayList();
+
+        arrayList.delete(1);
+        System.out.println("Custom array list after deletions: ");
+
+        showArrayList();
+
+        System.out.println("Get by index: " + arrayList.getByIndex(2));
+
+        System.out.println("Creating a customHashSet:");
+        HashSet hashSet = new HashSet();
+        hashSet.add(1);
+        hashSet.add(2);
+        hashSet.add(3);
+
+        System.out.println("Displaying custom hashset");
+        hashSet.display();
     }
 
+    public static void showArrayList() {
+        for(int i = 0; i < arrayList.getSize(); i++) {
+            System.out.println(arrayList.getByIndex(i));
+        }
+    }
 }
